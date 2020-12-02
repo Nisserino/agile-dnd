485924cf4bf4714740644826381505043372261e
Nisse <nielsen.rasmus@pm.me>
2020-12-02 15:04:41 +0100

    renamed new_game and load_game to new/load char, added start_game func

--- menue.py
+++ menue.py
@@ -5,12 +5,17 @@ class Menue_loop(cmd.Cmd):
     intro = 'Welcome to the dungeon run!'
     prompt = '-> '
 
-    def do_new_game(self, arg):
+    def do_new_character(self, arg):
         'Start a new game with a new character'
         pass
 
-    def do_load_game(self, arg):
+    def do_load_character(self, arg):
         'Load an existing character'
+        pass
+
+    def do_start_game(self, arg):
+        'Start the game'
+        # call func to let player decide on board size
         pass
 
     def do_quit(self, arg):
