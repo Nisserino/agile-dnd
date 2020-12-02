50a7d2a189a26b96515f2991376e1db11f3baf0c
Anders C <christensonwow@hotmail.com>
2020-12-02 14:08:52 +0100

    fix

--- enemysubclass.py
+++ enemysubclass.py
@@ -1,25 +1,31 @@
-class enemies:
-    class giantspider():
+class Entity:
+    pass
+
+    class Giantspider(Entity):
         def __init__(self):
             self.initiativ = 7
             self.endurance = 9
             self.attack = 2
             self.flexibility = 3
 
-    class skeleton:
+
+    class Skeleton(Entity):
         def __init__(self):
             self.initiativ = 4
             self.endurance = 2
             self.attack = 3
             self.flexibility = 3
     
-    class orc:
+
+    class Orc(Entity):
         def __init__(self):
             self.initiativ = 6
             self.endurance = 3
             self.attack = 4
             self.flexibility = 4
-    class troll:
+
+
+    class Troll(Entity):
         def __init__(self):
             self.initiativ = 2
             self.endurance = 4
