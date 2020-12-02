29c49699f45de79e12eb822c1ea69c647422869d
Anders C <christensonwow@hotmail.com>
2020-12-02 13:58:44 +0100

    beginning of enemy characters

---
+++ enemysubclass.py
@@ -0,0 +1,27 @@
+class enemies:
+    class giantspider():
+        def __init__(self):
+            self.initiativ = 7
+            self.endurance = 9
+            self.attack = 2
+            self.flexibility = 3
+
+    class skeleton:
+        def __init__(self):
+            self.initiativ = 4
+            self.endurance = 2
+            self.attack = 3
+            self.flexibility = 3
+    
+    class orc:
+        def __init__(self):
+            self.initiativ = 6
+            self.endurance = 3
+            self.attack = 4
+            self.flexibility = 4
+    class troll:
+        def __init__(self):
+            self.initiativ = 2
+            self.endurance = 4
+            self.attack = 7
+            self.flexibility = 2
