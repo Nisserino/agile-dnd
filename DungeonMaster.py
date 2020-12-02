d47bb7ac0566e83ff29380a4c38bf22da30050d8
Joel Lundin <joel.lundin@gmail.com>
2020-12-02 12:22:27 +0100

    Updated

--- DungeonMaster.py
+++ DungeonMaster.py
@@ -1,5 +1,15 @@
+# TODO: import monster
+# TODO: import skatter
+# TODO: import karta
+# TODO: import Dices
 
 
 class DungeonMaster:
     def __init__(self):
+        play_area = karta
+
+    def npc_spawner(self, entity):
+        pass
+
+    def item_spawner(self):
         pass
