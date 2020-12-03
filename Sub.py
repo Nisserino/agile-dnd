d11ac824aff6379ac22840ca5f73cb975e83f37d
burak-create <burakdogan@hotmail.se>
2020-12-03 05:09:03 +0100

    fixed

--- Sub.py
+++ Sub.py
@@ -1,14 +1,6 @@
 import random
 enemyAttackCount = 1 #temp variable
 
-
-#class Ability():
-#    def __init__(self, blindness, block, dubbelSkatt):
-#        self.blindness = blindness
-#        self.block = block
-#        self.dubbelSkatt = dubbelSkatt
-
-
 class Entity:
     def __init__(self, initiativ, endurance, attack, flexibility):
         self.initiativ = initiativ
@@ -35,8 +27,8 @@ class Wizard(Entity):
         self.lexibility = 5
 
     def blindness(self):
-        if random.randrange(0, 1) <= 0.8:
-            return "escape"
+        if random.randrange(0, 10) >= 8:
+            print("Player escaped from room!")
         
 
 class Thief(Entity):
@@ -48,12 +40,6 @@ class Thief(Entity):
 
     def dubbelAward(self):
         if random.randint(1, 4) == 4:
-            return "dubble award"
+            dungeonAward = dungeonAward * 2
+            print(f"Player gets 2X COINS = {dungeonAward}") #How we'll handle taxes?
             
- 
-
-#Rider = Heroes(5, 9, 6, 4, "Block first attack")
-#Wizard = Heroes(6, 4, 9, 5, "Blind 80%")
-#Tjuven = Heroes(7, 5, 5, 7, "%20 chans to more skatt")
-
-#HeroList = []
