a826adfca824bcd3463b6877aeb7bf1ab5f683f9
burak-create <burakdogan@hotmail.se>
2020-12-06 17:21:15 +0100

    added heroType

--- Sub.py
+++ Sub.py
@@ -1,12 +1,12 @@
 import random
-enemyAttackCount = 1 #temp variable
 
 class Entity:
-    def __init__(self, initiativ, endurance, attack, flexibility):
+    def __init__(self, initiativ, endurance, attack, flexibility, heroType):
         self.initiativ = initiativ
         self.endurance = endurance
         self.attack = attack
         self.flexibility = flexibility
+        self.heroType = heroType
 
 class Rider(Entity):
     def __init__(self):
@@ -14,9 +14,10 @@ class Rider(Entity):
         self.endurance = 9
         self.attack = 6
         self.lexibility = 4
+        self.heroType ='Rider'
 
     def blockAttack(self):
-        self.x = enemyAttackCount - 1
+        pass
         
 
 class Wizard(Entity):
@@ -25,9 +26,10 @@ class Wizard(Entity):
         self.endurance = 4
         self.attack = 9
         self.lexibility = 5
+        self.heroType ='Wizard'
 
     def blindness(self):
-        if random.randrange(0, 10) >= 8:
+        if random.randrange(0, 10) <= 8:
             print("Player escaped from room!")
         
 
@@ -37,9 +39,9 @@ class Thief(Entity):
         self.endurance = 9
         self.attack = 6
         self.lexibility = 4
+        self.heroType ='Thief'
 
     def dubbelAward(self):
         if random.randint(1, 4) == 4:
             dungeonAward = dungeonAward * 2
             print(f"Player gets 2X COINS = {dungeonAward}") #How we'll handle taxes?
-            
