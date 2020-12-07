dd4e316545224d262bc9bd89ad5aa6e9ddd42937
Nisse <nielsen.rasmus@pm.me>
2020-12-07 16:41:02 +0100

    testing to add dices in order to get around merge conflict with build

---
+++ dices.py
@@ -0,0 +1,10 @@
+import random
+
+
+class Dices:
+
+    def d6(self, rolls):
+        total_sum = 0
+        for i in range(rolls):
+            total_sum += random.randint(1, 6)
+        return total_sum
