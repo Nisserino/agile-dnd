b22ee1a2b0d4a27219ea562ffebd6016af3dcd0b
Nisse <nielsen.rasmus@pm.me>
2020-12-08 19:15:40 +0100

    Fix CharacterCreation class

--- ChCreation.py
+++ ChCreation.py
@@ -4,11 +4,13 @@ import saveLoad as s
 
 class CharacterCreation:
     def __init__(self, username, choice):
-        self.username = ''
+        self.username = username
         self.hero = None
         self.choice = choice
+        self.create_instance()
+        self.send_to_save((self.username, self.hero))
 
-    def user(self):
+    def create_instance(self):
         if self.choice == 'Knight':
             self.hero = e.Knight()
         elif self.choice == 'Wizard':
@@ -16,4 +18,5 @@ class CharacterCreation:
         elif self.choice == 'Thief':
             self.hero = e.Thief()
 
-        s.FileHandler.save(self.username, self.hero)
+    def send_to_save(self, data):
+        s.FileHandler().save(data)
