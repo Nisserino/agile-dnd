5e38e6cf3290e61b2cf22d8385dca8c618505afc
Nisse <nielsen.rasmus@pm.me>
2020-12-08 16:50:56 +0100

    add charachter creation loop

--- menue.py
+++ menue.py
@@ -1,4 +1,5 @@
 import cmd
+from ChCreation import CharacterCreation as CC
 
 
 class Menue_loop(cmd.Cmd):
@@ -6,8 +7,8 @@ class Menue_loop(cmd.Cmd):
     prompt = '-> '
 
     def do_new_character(self, arg):
-        'Start a new game with a new character'
-        pass  # add func call when class is ready
+        'Create a new character'
+        CharacterCreationLoop().cmdloop()
 
     def do_load_character(self, arg):
         'Load an existing character'
@@ -21,6 +22,48 @@ class Menue_loop(cmd.Cmd):
     def do_quit(self, arg):
         'Exit the game'
         return True  # Exits the loop
+
+
+class CharacterCreationLoop(cmd.Cmd):
+    intro = 'What would you like to play as?'
+    prompt = '-> '
+
+    def __init__(self):
+        super().__init__()
+        self.taken_names = ('pelle')
+
+    def do_wizard(self, arg):
+        'Create a wizard'
+        user = self.get_username()
+        if user:
+            CC('Wizard', user)
+            return True
+
+    def do_knight(self, arg):
+        'Create a knight'
+        user = self.get_username()
+        if user:
+            CC('Knight', user)
+            return True
+
+    def do_theif(self, arg):
+        'Create a thief'
+        user = self.get_username()
+        if user:
+            CC('Knight', user)
+            return True
+
+    def get_username(self):
+        username = input('What are you called?')
+        check = self.check_username(username)
+        if check:
+            return username
+        else:
+            return False
+
+    def check_username(self, username):
+        if username in self.taken_names:
+            print('That username is already taken!')
 
 
 if __name__ == "__main__":
