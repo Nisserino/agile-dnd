bb6e1918644d8aa2e181926fea91bcc18685d62f
burak-create <burakdogan.hotmail.se>
2020-12-09 18:56:51 +0100

    close the previous window

--- testgui.py
+++ testgui.py
@@ -17,11 +17,14 @@ class GuiTools():
     def __init__(self, master):
         self.master = master
 
-    def new_window(self):
+    def new_window(self):  # not in use
         self.window = tk.Toplevel(self.master)
-        self.window.title = ('')
+        self.window.title('')
         self.window.iconbitmap('icon.ico')
         self.window.geometry('800x600')
+
+    def closeWindow(self):
+        self.windowReg.destroy()
 
     def popUp(self):
         self.choice4_btn = messagebox.askyesno('Quit the game', 'Are you sure?')
@@ -34,37 +37,41 @@ class GuiTools():
 
     def registerationWindow(self):
         # creates new window
-        self.window = tk.Toplevel(self.master)
-        self.window.title = ('Registeration')
-        self.window.iconbitmap('icon.ico')
-        self.window.geometry('800x600')
+        self.windowReg = tk.Toplevel(self.master)
+        self.windowReg.title('Registeration')
+        self.windowReg.iconbitmap('icon.ico')
+        self.windowReg.geometry('800x600')
         # label / entry / button for username reg.
-        self.usernameLabel = tk.Label(self.window, text='Write down a unique name')
+        self.usernameLabel = tk.Label(self.windowReg, text='Write down a unique name')
         self.usernameLabel.pack()
-        self.username_txt = tk.Entry(self.window, width=30)
+        self.username_txt = tk.Entry(self.windowReg, width=30)
         self.username_txt.pack(pady=10)
-        self.regButton = tk.Button(self.window, text='Register', command=lambda: g.mainMenu())
+        self.regButton = tk.Button(self.windowReg, text='Register', command=lambda: [self.mainMenu(), self.closeWindow()])
         self.regButton.pack()
+        # test shit
+        # tk.Label(self.windowReg, text=self.regButton).pack()
 
     def mainMenu(self):
         self.username = self.username_txt.get()  # connect with chcreation username
         print(self.username)  # test shit
         # creates new window
-        self.window = tk.Toplevel(self.master)
-        self.window.title = ('Main Menu')
-        self.window.iconbitmap('icon.ico')
-        self.window.geometry('800x600')
+        self.windowMenu = tk.Toplevel(self.master)
+        self.windowMenu.title('Main Menu')
+        self.windowMenu.iconbitmap('icon.ico')
+        self.windowMenu.geometry('800x600')
         # radio buttons / buttons for choices
         # -----------BUTTONS
-        self.choice1_btn = tk.Button(self.window, text='Create a new character', command=lambda: 'Menu_loop.do_new_character()')
+        self.choice1_btn = tk.Button(self.windowMenu, text='Create a new character', command=lambda: 'Menu_loop.do_new_character()')
         self.choice1_btn.grid(row=6, column=1, columnspan=2, padx=10, pady=10, ipadx=70)
-        self.choice2_btn = tk.Button(self.window, text="Load an existing character", command=lambda: 'Menu_loop.do_load_character()')
+        self.choice2_btn = tk.Button(self.windowMenu, text="Load an existing character", command=lambda: 'Menu_loop.do_load_character()')
         self.choice2_btn.grid(row=7, column=1, columnspan=2, padx=10, pady=10, ipadx=55)
-        self.choice3_btn = tk.Button(self.window, text="Start the game", command=lambda: 'Menu_loop.do_start_game()')
+        self.choice3_btn = tk.Button(self.windowMenu, text="Start the game", command=lambda: 'Menu_loop.do_start_game()')
         self.choice3_btn.grid(row=8, column=1, columnspan=2, padx=10, pady=10, ipadx=40)
-        self.choice5_btn = tk.Button(self.window, text='Quit the game', command=lambda: g.popUp())
+        self.choice5_btn = tk.Button(self.windowMenu, text='Quit the game', command=lambda: self.popUp())
         self.choice5_btn.grid(row=9, column=1, columnspan=2, padx=10, pady=10, ipadx=40)
 
+        # test shit
+        # tk.Label(self.windowMenu, text=self.choice1_btn).grid(row=15, column=1, columnspan=2, padx=10, pady=10, ipadx=40)
 
         # ch creation button for every each hero
         # message button for quit
