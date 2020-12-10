b5e910ff0b6d8ac830b96b9c73e2f90fdf4d99fa
Nisse <nielsen.rasmus@pm.me>
2020-12-10 11:20:26 +0100

    add more framework, remove debugging lines

--- game_loop.py
+++ game_loop.py
@@ -109,23 +109,32 @@ class CombatLoop(cmd.Cmd):
         self.dm = dm
         self.username = username
         self.attack_order = []
+        self.enemies = []
         self.next_loop = 'move'
 
     def do_attack(self, arg):
+        'Attack, if there are more targets, you get to choose'
         pass
 
     def do_hp(self, arg):
+        'Show the HP of everyone'
         pass
 
     def do_stats(self, arg):
         pass
 
-    def do_quit(self, arg):
-        return True
+    def do_escape(self, arg):
+        'Attemt to run away from the fight'
+        if self.escape():
+            return True
 
 #   - - - Unseen funcs - - -
     def deal_damage(self, attacker, blocker):
-        pass
+        if attacker.attack_roll() > blocker.evade_roll():
+            blocker.take_hit()
+            print(f'{attacker.name} landed a hit on {blocker.name}')
+        else:
+            print(f'{blocker.name} managed to evade the attack!')
 
     def init_rolls(self):
         pass
@@ -134,15 +143,16 @@ class CombatLoop(cmd.Cmd):
         # if self.dm.
         pass
 
+    def escape(self):
+        if self.dm.player.escape_roll():
+            print('Managed to escape from the enemies')
+            return True
+        else:
+            print('You slipped trying to run, and didn\'t manage to escape')
+
 #   - - - Cmd specific funcs - - -
     def preloop(self):
         self.dm.entity_spawner(self.dm.player.position)
 
     def postloop(self):
         Bouncer(self.dm, self.username, self.next_loop)
-
-
-import entities
-p = entities.Knight()
-u = 'aa'
-Startup(p, u, 4, [0, 0])
