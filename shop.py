140a98641be962390efc674e6dcffc2633c902b6
Olle Sundin <ollesundin.universitet@gmail.com>
2020-12-10 14:51:47 +0100

    remove loop

--- shop.py
+++ shop.py
@@ -34,58 +34,7 @@ class Shop:
         print()
 
     def shop_loop(self):
-        shopping = True
-        self.menu()
-        while shopping:
-            option = int(input(
-                'Enter a number of the item you want to purchase: '
-            ))
-            if option >= 5:
-                if option == 5:
-                    self.menu()
-                elif option == 6:
-                    shopping = False
-                    self.checkout()
-                else:
-                    print('That item is not available')
-            elif self.removed[option-1] == 1:
-                print("""
-                This item is already in your inventory.
-                You can only purchase this item once a run.
-                """)
-            elif option == 1:
-                print('Small potion is ready for checkout!')
-                print()
-                self.checkout_items.append(
-                    [self.available_items.get('Small potion')])
-                self.inventory.append(['Small potion', self.s_potion])
-            elif option == 2:
-                print('Big potion is ready for checkout!')
-                print()
-                self.removed[1] += 1
-                self.checkout_items.append(
-                    [self.available_items.get('Big potion')]
-                )
-                del self.available_items['Big potion']
-                self.inventory.append(['Big potion', self.b_potion])
-            elif option == 3:
-                print('Sword is ready for checkout!')
-                print()
-                self.removed[2] += 1
-                self.checkout_items.append(
-                    [self.available_items.get('Sword')]
-                )
-                del self.available_items['Sword']
-                self.inventory.append(['Sword', self.sword])
-            elif option == 4:
-                print('Body Armor is ready for checkout!')
-                print()
-                self.removed[3] += 1
-                self.checkout_items.append(
-                    [self.available_items.get('Body Armor')]
-                )
-                del self.available_items['Body Armor']
-                self.inventory.append(['Body Armor', self.armor])
+        pass
 
     def checkout(self):
         os.system('cls')
@@ -100,7 +49,3 @@ class Shop:
 
     def remove_item(self):
         pass
-
-
-shop = Shop('player1')
-shop.shop_loop()
