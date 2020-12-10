d93f5abe0c0551d3f44d996452b29d80e72becc4
Joel Lundin <joel.lundin@gmail.com>
2020-12-10 11:29:46 +0100

    Added docstrings

--- saveLoad.py
+++ saveLoad.py
@@ -4,10 +4,12 @@ import pickle
 class FileHandler:
 
     def save(self, data):
+        '''Pickles data and saves it to a file'''
         with open('savefile.pickle', 'wb') as fout:
             pickle.dump(data, fout)
 
     def load(self):
+        '''Load data from a file and depickle it. Returns a dict'''
         try:
             with open('savefile.pickle', 'rb') as fin:
                 data = pickle.load(fin)
@@ -22,9 +24,11 @@ class DataHandler:
         self.character_data = {}
 
     def update(self, data: tuple):
+        '''Update the character collection with a tuple of username and hero instance'''
         self.character_data[data[0]] = data[1]
 
     def get_names(self) -> list:
+        '''Returns a list of all the used usernames'''
         name_list = []
         for c in self.character_data:
             name_list.append(c)
@@ -32,4 +36,5 @@ class DataHandler:
         return name_list
 
     def load_character(self, name: str):
+        '''Load a character'''
         return self.character_data[name]
