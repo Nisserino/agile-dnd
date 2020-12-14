62ce042c35be368450039ec5647d64b912af7e49
burak-create <burakdogan.hotmail.se>
2020-12-14 15:52:46 +0100

    modified

--- testgui.py
+++ testgui.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import ImageTk, Image
 from tkinter import messagebox
+import DungeonMaster as DM
+from roomdesc import RoomDescription
+from game_loop import CombatLoop
+import menue as M
+import subprocess
+
 
 root = tk.Tk()
 root.title('D&D')
@@ -19,44 +25,17 @@ root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 logo = ImageTk.PhotoImage(Image.open('dnd.jpg'))
 imgLabel = tk.Label(image=logo)
 imgLabel.pack()
-startButton = tk.Button(root, text='Start the game!', command=lambda: g.registerationWindow())
+startButton = tk.Button(root, text='Start the game!', command=lambda: g.mainMenu())
 startButton.pack(pady=30)
 logo1 = ImageTk.PhotoImage(Image.open('heroes.jpg'))
 imgLabel1 = tk.Label(image=logo1)
 imgLabel1.pack()
 
+# ======================================= # ============================ #
+# combat = CombatLoop()
+# pickBoard = PickBoardSize()
+
 a = '''
-                                            /-\                       /-\            ... Room
-                       H                    | |                       | | Room       . . not on
-                                            \#/                       \-/            ... map
-                                             |              
-                                      /-\   /#\   /-\                 /-\
-                        G              | |-O-| #---+ |                 |:| Dark room
-                                        \+/   \+/   \-/                 \-/
-                                        |     X
-                                /-\   ...   /+\                       /-\
-                        F        | |   .?.---+ |                       |?| Special room
-                                \+/   ...   \-/                       \-/
-                                    |           O
-                            /-\   /#\   /-\   /-\   /-\                 X   Locked door
-                        E  |?|-O-| |   |?|   | #---# |                 #   Magic door
-                            \+/   \-/   \+/   \+/   \-/                 O   Bombable wall
-                            |     O     |     X
-                            /+\   /-\   /#\   /+\                       4A - Entrance
-                        D  |:#---# |   | +-X-+ |                       1E, 3E - Hints
-                            \-/   \-/   \+/   \+/                       5C - Compass
-                                        |     |                        4F - Map
-                                /-\   /+\   /#\   /-\                 3F - 10 rupies
-                        C        | #---+:+---# +-X-+:|                 2C, 3C, 4C, 1D,
-                                \-/   \-/   \-/   \-/                   2D, 5A - Keys
-                                                O                        2A - Stair to MAGIC BOOK
-                                            /-\                       5G - Stair to MAGIC KEY
-                        B                    | |                       5E - Stair to 1D
-                                            \+/                       2F - Triforce
-                                                |
-                                /-\   /-\   /+\   /-\
-                        A        | +---# +---+ +---+:|
-                                \-/   \-/   \+/   \-/
 '''
 
 
@@ -70,13 +49,15 @@ class GuiTools():
         self.x = (self.ws/2) - (self.width/2)
         self.y = (self.hs/2) - (self.height/2)
 
+    def testFunc(self):
+        pass
+
     def areYouSure(self):
-        self.response = messagebox.askyesno('Start the game', 'Are you ready?')
+        self.response = messagebox.askyesno('Hero selection', 'Are you ready?')
 
         if self.response == 1:
             self.windowCh.destroy()
-            self.windowMenu.destroy()
-            return self.gameWindow()
+            return self.registerationWindow()
         else:
             return self.windowCh
 
@@ -85,11 +66,9 @@ class GuiTools():
 
         if self.choice4_btn == 1:
             root.destroy()
-            print("quit")
-            # return Menu_loop.do_quit(True)
+            # print("quit")
         else:
-            print('Dont quit')
-            self.windowMenu.destroy()
+            # print('Dont quit')
             return self.mainMenu()
 
     def registerationWindow(self):
@@ -113,13 +92,8 @@ class GuiTools():
         # tk.Label(self.windowReg, text=self.regButton).pack()
 
     def usernameHandler(self):  # Checks user name
-        if self.username_txt.get():
-            # self.username = self.username_txt.get()  # connect with chcreation username
-            # print(self.username)  # test shit
-            print(self.username_txt.get())
-            return self.mainMenu()
-        else:
-            return self.mainMenu()
+        self.child = subprocess.Popen(['python', 'menue.py'], stdin=subprocess.PIPE)
+        self.child.communicate(self.username_txt.get())
 
     def newChCreation(self):
         self.windowCh = tk.Toplevel(self.master)
@@ -131,9 +105,9 @@ class GuiTools():
         self.logoCh = ImageTk.PhotoImage(Image.open('heroes1.jpg'))
         self.imgLabelCh = tk.Label(self.windowCh, image=self.logoCh).pack()
         # =============Hero creation buttons========
-        self.wizardBtn = tk.Button(self.windowCh, text='Wizard', command=lambda: [self.areYouSure(), 'do_wizard'])
-        self.knightBtn = tk.Button(self.windowCh, text='Knight', command=lambda: [self.areYouSure(), 'do_knight'])
-        self.thiefBtn = tk.Button(self.windowCh, text='Thief', command=lambda: [self.areYouSure(), 'do_thief'])
+        self.wizardBtn = tk.Button(self.windowCh, text='Wizard', command=lambda: [self.areYouSure() , chCreate.do_wizard('wizard')])
+        self.knightBtn = tk.Button(self.windowCh, text='Knight', command=lambda: [self.areYouSure(), chCreate.do_knight('knight')])
+        self.thiefBtn = tk.Button(self.windowCh, text='Thief', command=lambda: [self.areYouSure(), chCreate.do_theif('theif')])
         # ============Buttons placement============
         self.wizardBtn.pack(pady=5, ipadx=70)
         self.knightBtn.pack(pady=5, ipadx=70)
@@ -149,7 +123,7 @@ class GuiTools():
         # =======Show saved files============
         self.loadBtn = []
         for i in range(10):
-            self.loadBtn.append(tk.Button(self.windowLoad, text='Load file ' + str(i+1), command=lambda i=i: 'load_character, self.open_this(i)'))
+            self.loadBtn.append(tk.Button(self.windowLoad, text='Load file ' + str(i+1), command=lambda i=i: 'Menue_loop.do_load_character, self.open_this(i)'))
             self.loadBtn[i].pack()  # grid(column=4, row=i+1, sticky='w')
         # =======Load and Quit Buttons=======
         # self.loadBtn = tk.Button(self.windowLoad, text='LOAD', command=lambda: '')
@@ -165,13 +139,13 @@ class GuiTools():
         self.windowMenu.geometry('%dx%d+%d+%d' % (w, h, x, y))
         # radio buttons / buttons for choices
         # -----------BUTTONS
-        self.choice1_btn = tk.Button(self.windowMenu, text='Create a new character', command=lambda: self.newChCreation())
+        self.choice1_btn = tk.Button(self.windowMenu, text='Create a new character', command=lambda: [self.newChCreation(), M.Menue_loop().onecmd('new_character')])
         self.choice1_btn.pack(padx=10, pady=10, ipadx=70)
-        self.choice2_btn = tk.Button(self.windowMenu, text="Load an existing character", command=lambda: ['Menu_loop.do_load_character()', self.loadCharacter()])
+        self.choice2_btn = tk.Button(self.windowMenu, text="Load an existing character", command=lambda: [self.loadCharacter(), M.Menue_loop().onecmd('load_character')])
         self.choice2_btn.pack(padx=10, pady=10, ipadx=59)
-        self.choice3_btn = tk.Button(self.windowMenu, text="Start the game", command=lambda: ['Menu_loop.do_start_game()', self.windowMenu.destroy(), self.gameWindow()])
+        self.choice3_btn = tk.Button(self.windowMenu, text="Start the game", command=lambda: [M.Menue_loop().onecmd('start_game'), self.mapSize(), self.windowMenu.destroy(), M.Menue_loop().onecmd('start_game')])
         self.choice3_btn.pack(padx=10, pady=10, ipadx=90)
-        self.choice5_btn = tk.Button(self.windowMenu, text='Quit the game', command=lambda: self.popUp())
+        self.choice5_btn = tk.Button(self.windowMenu, text='Quit the game', command=lambda: [self.popUp(), M.Menue_loop().onecmd('quit')])
         self.choice5_btn.pack(padx=10, pady=10, ipadx=90)
 
         # test shit
@@ -182,6 +156,27 @@ class GuiTools():
         # message button for start! R u ready? Y/N
         # return gamemap()
         pass
+
+    def mapSize(self):
+        # creates new window
+        # self.mapSizeWindow = tk.Toplevel(self.master)
+        # self.mapSizeWindow.title('Chosing map size')
+        # self.mapSizeWindow.iconbitmap('icon.ico')
+        # self.mapSizeWindow.configure(background='#89001c')
+        # self.mapSizeWindow.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        # self.map = tk.IntVar()
+        # self.smallMap = tk.Radiobutton(self.mapSizeWindow(), text='', variable=self.map, value=1).pack()
+        # self.mediumMap = tk.Radiobutton(self.mapSizeWindow(), text='', variable=self.map, value=2).pack()
+        # self.bigMap = tk.Radiobutton(self.mapSizeWindow(), text='', variable=self.map, value=3).pack()
+
+        # if self.map.get() == 1:
+        #     return pickBoard.do_small()
+        # elif self.map.get() == 2:
+        #     return pickBoard.do_medium()
+        # else:
+        #     return pickBoard.do_big()
+        return self.gameWindow()
 
     def gameWindow(self):
         # creates new window
@@ -198,10 +193,10 @@ class GuiTools():
         self.eastBtn = tk.Button(self.windowDnd, text='Go to East', command=lambda: 'do_east')
         self.westBtn = tk.Button(self.windowDnd, text='Go to West', command=lambda: 'do_west')
         # ====Packing Buttons===
-        self.eastBtn.pack(pady=10)
-        self.westBtn.pack(pady=10)
-        self.southBtn.pack(pady=10)
-        self.northBtn.pack(pady=10)
+        self.eastBtn.pack(anchor='e', pady=10)
+        self.westBtn.pack(anchor='w', pady=10)
+        self.southBtn.pack(anchor='s', pady=10)
+        self.northBtn.pack(anchor='n', pady=10)
 
         # =====If enemy founds:
         return self.battle()
@@ -210,11 +205,11 @@ class GuiTools():
 
     def buyItems(self):  # ============TESTING===================
         self.varList = [self.variableHp.get(), self.bigPotVar.get(), self.armorVar.get(), self.swordVar.get()]
-        for i in self.varList:
-            if self.varList[i] == 1:
-                print('You bought')
-            else:
-                return self.windowShop
+        self.counter = 0
+        for i in range(len(self.varList)):
+            if self.varList[i] == 'On':
+                self.counter += 1
+        self.boughtLabel = tk.Label(self.windowShop, text='You bought ' + str(self.counter) + ' item/s').pack()
 
     def shop(self):
         # =======Create new window
@@ -241,23 +236,24 @@ class GuiTools():
         self.goBackBtn.pack()
         # ======Check List=====
         self.hpPotSmall = tk.Checkbutton(self.windowShop, text='Small Hp potion')
-        self.variableHp = tk.IntVar()
-        self.hpPotSmall.config(variable=self.variableHp)
+        self.variableHp = tk.StringVar()
+        self.hpPotSmall.config(variable=self.variableHp, onvalue='On', offvalue='Off')
+        self.hpPotSmall.deselect()
         self.hpPotSmall.pack()  # Replace=======
-
         self.hpPotBig = tk.Checkbutton(self.windowShop, text='Big Hp potion')
-        self.bigPotVar = tk.IntVar()
-        self.hpPotBig.config(variable=self.bigPotVar)
+        self.bigPotVar = tk.StringVar()
+        self.hpPotBig.config(variable=self.bigPotVar, onvalue='On', offvalue='Off')
+        self.hpPotBig.deselect()
         self.hpPotBig.pack()  # Replace=======
-
         self.bodyArmor = tk.Checkbutton(self.windowShop, text='Body armor')
-        self.armorVar = tk.IntVar()
-        self.bodyArmor.config(variable=self.armorVar)
+        self.armorVar = tk.StringVar()
+        self.bodyArmor.config(variable=self.armorVar, onvalue='On', offvalue='Off')
+        self.bodyArmor.deselect()
         self.bodyArmor.pack()  # Replace=======
-
         self.sword = tk.Checkbutton(self.windowShop, text='Sword')
-        self.swordVar = tk.IntVar()
-        self.sword.config(variable=self.swordVar)
+        self.swordVar = tk.StringVar()
+        self.sword.config(variable=self.swordVar, onvalue='On', offvalue='Off')
+        self.sword.deselect()
         self.sword.pack()  # Replace=======
 
     def defeat(self):
@@ -271,21 +267,21 @@ class GuiTools():
         # quit/ play again close all windows
 
     def hpHandler(self):
-        # ======Hp bar===============(testing)--
-        self.heroHpLabel = tk.Label(self.windowBattle, text='Your health points').pack(side='left')
+        # ======Hp bar===============(testing)
+        self.heroHpLabel = tk.Label(self.windowBattle, text='Your health points').pack(anchor='w')
         self.heroHP = ttk.Progressbar(self.windowBattle, orient='horizontal', length=200, mode='determinate')
-        self.heroHP.pack(side='left')
-        self.monsterHpLabel = tk.Label(self.windowBattle, text='Enemy health points').pack(side='right')
+        self.heroHP.pack(anchor='w')
         # =======Multipple Hp bar for monsters=
         self.monsterHPBars = []
-        for i in range(10):
+        for i in range(4):
+            self.monsterHPLabel = tk.Label(self.windowBattle, text='Monster health points').pack(anchor='e')
             self.monsterHPBars.append(ttk.Progressbar(self.windowBattle, orient='horizontal', length=200, mode='determinate'))
-            self.monsterHPBars[i].pack()  # grid(column=4, row=i+1, sticky='w')
+            self.monsterHPBars[i].pack(anchor='e')  # grid(column=4, row=i+1, sticky='w')
+            self.monsterHPBars[i]['value'] = 100  # monster.endurance*10
         # =====Fill the hp bars
         self.heroHP['value'] = 100  # hero.endurance*10
-        self.monsterHP['value'] = 0  # monster.endurance*10
 
-        if self.heroHP['value'] > 0 and self.monsterHP['value'] == 0:
+        if self.heroHP['value'] > 0 and self.monsterHPBars['value'] == 0:
             self.answer = messagebox.showinfo('Victory!', 'You won!')
             if self.answer.lower() == 'ok':
                 self.windowBattle.destroy()
@@ -298,8 +294,8 @@ class GuiTools():
                 self.windowDnd.destroy()
                 return self.defeat()
 
-    def checkStats(self):  # To check stats in dungeon
-        self.statsLabel = tk.Label(self.windowBattle, text='TESTSSSSSSSdo_statsSSSSSSSSSSSS').pack()
+    # def checkStats(self):  # To check stats in dungeon
+    #     self.statsLabel = tk.Label(self.windowBattle, text='combat.get_stats'()).pack()
 
     def battle(self):
         self.windowBattle = tk.Toplevel(self.master)
@@ -319,13 +315,15 @@ class GuiTools():
         sdlkfsdlkfjsdflksdf;sdlfksdf;lkf;sdkfsd;fks;d
         ;sldkfsd;lfksd;flksdf;lskdf;lsfdks;dflksd;fks
         '''
-        self.descLabel = tk.Label(self.windowBattle, text=self.randomdesc, bg='black', fg='white').pack()
+        self.descVar = tk.StringVar()
+        self.descVar.set(RoomDescription().get_description())
+        self.descLabel = tk.Label(self.windowBattle, textvariable=self.descVar, bg='black', fg='white').pack()
         # ======Dungeon Buttons======
         self.attackBtn = tk.Button(self.windowBattle, text='ATTACK!', command=lambda: [self.hpHandler(), self.statusLabel.destroy(), 'do_attack()'])
         self.escapeBtn = tk.Button(self.windowBattle, text='ESCAPE!', command=lambda: [self.shop(), self.statusLabel.destroy(), 'do_escape()'])
         self.statsBtn = tk.Button(self.windowBattle, text='See Stats!', command=lambda: [self.descLabel.destroy(), self.statusLabel.destroy(), self.checkStats()])
         # ======Dungeon Labels=======
-        self.statusLabel = tk.Label(self.windowBattle, text='DungeonMaster.room')
+        self.statusLabel = tk.Label(self.windowBattle, text='DM.DungeonMaster.print_room_status()')
         # ======Packing Buttons======
         self.attackBtn.pack()
         self.escapeBtn.pack()
@@ -344,6 +342,7 @@ class GuiTools():
             #return self.gameWindow()
 
 
-
 g = GuiTools(root)
+menu = M.Menue_loop()
+chCreate = M.CharacterCreationLoop(menu.data_handler)
 root.mainloop()
